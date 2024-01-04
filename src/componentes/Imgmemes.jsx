0c5107e1ec042f9064e72febde84224d39288e5f
Soledad Shukuya <soledadshukuya@hotmail.com>
2024-01-04 18:49:45 -0300

    Terminado

--- src/componentes/Imgmemes.jsx
+++ src/componentes/Imgmemes.jsx
@@ -51,19 +51,19 @@ export default function Imgmemes() {
 
     return (
         <div className="text-center">
-            <nav className="navbar navbar-expand-lg navbar-dark">
-                <div className="container-fluid align-items-end ">
+            <nav class="navbar navbar-dark navbar-expand-lg">
+                <div class="container-fluid">
                     <h1>Editor de Memes</h1>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div className="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                        <div className="offcanvas-header">
+                    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                        <div class="offcanvas-header">
                             <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Editor de Memes</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                         </div>
-                        <div className="offcanvas-body">
-                            <ul className="navbar-nav justify-content-end text-start flex-grow-1 pe-3">
+                        <div class="offcanvas-body">
+                            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                                 <li className="nav-item ">
                                     <a className="nav-link active" aria-current="page" href="https://github.com/solshk/cac-23644-editor-de-memes">Repo</a>
                                 </li>
@@ -76,75 +76,77 @@ export default function Imgmemes() {
                 </div>
             </nav>
 
+            <main>
+                <div className="container">
+                    <div className="meme-container">
+                        <figure id="exportar">
+                            {textosMeme.map((val) =>
+                                <p className="texto-meme" style={{ fontSize: `${val.fontSize}px`, transform: `translate(${val.ejex}px, ${val.ejey}px)`, color: `${val.color}`, fontFamily: `${val.fontFamily}` }}>{val.texto}</p>
+                            )}
+                            <img src={imgmeme} className="d-block" style={{ height: "100%", width: "100%" }} alt="meme nuevo" />
+                        </figure>
+                    </div>
 
-            <div className="container">
-                <div className="meme-container">
-                    <figure id="exportar">
-                        {textosMeme.map((val) =>
-                            <p className="texto-meme" style={{ fontSize: `${val.fontSize}px`, transform: `translate(${val.ejex}px, ${val.ejey}px)`, color: `${val.color}`, fontFamily: `${val.fontFamily}` }}>{val.texto}</p>
-                        )}
-                        <img src={imgmeme} className="d-block" style={{ height: "100%", width: "100%" }} alt="meme nuevo" />
-                    </figure>
-                </div>
 
+                    <div className="editor-container">
 
-                <div className="editor-container">
+                        <div className="elegir-imagen-container">
+                            <h4>Seleccioná otra imagen:</h4>
+                            <div className="galeria-container">
+                                {memes.map(meme => (
+                                    <div className="contenedor-img" key={meme.id}>
+                                        <img src={meme.url} className="img" onClick={seleccionarImg} alt={meme.name} />
+                                    </div>))
+                                }
+                            </div>
+                        </div>
 
-                    <div className="elegir-imagen-container">
-                        <h4>Seleccioná otra imagen:</h4>
-                        <div className="galeria-container">
-                            {memes.map(meme => (
-                                <div className="contenedor-img" key={meme.id}>
-                                    <img src={meme.url} className="img" onClick={seleccionarImg} alt={meme.name} />
-                                </div>))
+                        <div className="texto-container">
+                            <div className="editor-row">
+                                <h4>Ingresa el texto:</h4>
+                                <button className="btn btn-primary" id="btn-colorcito" onClick={handleClick}>+</button>
+                            </div>
+
+                            {textosMeme.map((val, i) =>
+                                <div>
+                                    <div className="editor-row">
+                                        <input name="texto" className="form-control" type="text" placeholder="Ingresá el texto acá" value={val.texto} onChange={(e) => handleChange(e, i)} />
+                                        <input name="color" type="color" className="color" value={val.color} onChange={(e) => handleChange(e, i)} />
+                                        <button className="btn btn-primary" id="btn-colorcito" onClick={() => handleDelete(i)}>x</button>
+                                    </div>
+
+                                    <div className="editor-row">
+                                        <div>
+                                            <label>Ubicación horizontal:
+                                                <input name="ejex" type="range" min="0" max="400" value={val.ejex} onChange={(e) => handleChange(e, i)} />
+                                            </label>
+                                            <label>Ubicación vertical:
+                                                <input name="ejey" type="range" min="0" max="600" value={val.ejey} onChange={(e) => handleChange(e, i)} />
+                                            </label>
+                                        </div>
+                                        <div>
+                                            <label style={{ marginBottom: "1rem", marginRight: ".5rem" }}>Fuente:</label>
+                                            <select name="fontFamily" value={val.fontFamily} onChange={(e) => handleChange(e, i)}>
+                                                <option value={"Verdana"}>Verdana</option>
+                                                <option value={"Impact"}>Impact</option>
+                                                <option value={"Arial"}>Arial</option>
+                                            </select>
+
+                                            <label>Tamaño de la letra</label>
+                                            <input name="fontSize" type="range" min="10" max="100" value={val.fontSize} onChange={(e) => handleChange(e, i)} />
+                                        </div>
+                                    </div>
+                                </div>
+                            )
                             }
                         </div>
                     </div>
-
-                    <div className="texto-container">
-                        <div className="editor-row">
-                            <h4>Ingresa el texto:</h4>
-                            <button className="btn btn-primary" id="btn-colorcito" onClick={handleClick}>+</button>
-                        </div>
-
-                        {textosMeme.map((val, i) =>
-                            <div>
-                                <div className="editor-row">
-                                    <input name="texto" className="form-control" type="text" placeholder="Ingresá el texto acá" value={val.texto} onChange={(e) => handleChange(e, i)} />
-                                    <input name="color" type="color" className="color" value={val.color} onChange={(e) => handleChange(e, i)} />
-                                    <button className="btn btn-primary" id="btn-colorcito" onClick={() => handleDelete(i)}>x</button>
-                                </div>
-
-                                <div className="editor-row">
-                                    <div>
-                                        <label>Ubicación horizontal:
-                                            <input name="ejex" type="range" min="0" max="400" value={val.ejex} onChange={(e) => handleChange(e, i)} />
-                                        </label>
-                                        <label>Ubicación vertical:
-                                            <input name="ejey" type="range" min="0" max="600" value={val.ejey} onChange={(e) => handleChange(e, i)} />
-                                        </label>
-                                    </div>
-                                    <div>
-                                        <label style={{ marginBottom: "1rem", marginRight: ".5rem" }}>Fuente:</label>
-                                        <select name="fontFamily" value={val.fontFamily} onChange={(e) => handleChange(e, i)}>
-                                            <option value={"Verdana"}>Verdana</option>
-                                            <option value={"Impact"}>Impact</option>
-                                            <option value={"Arial"}>Arial</option>
-                                        </select>
-
-                                        <label>Tamaño de la letra</label>
-                                        <input name="fontSize" type="range" min="10" max="100" value={val.fontSize} onChange={(e) => handleChange(e, i)} />
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                        }
-                    </div>
                 </div>
-            </div>
+            
 
-            <button onClick={descargar} type="button" className="btn btn-primary" id="btn-descargar">Descargar</button>
-
+                <button onClick={descargar} type="button" className="btn btn-primary" id="btn-descargar">Descargar</button>
+            </main>
+            
             <footer>
                 <p className="footer__creditos">Hecho con ❤ por <a href="https://github.com/solshk">solshk</a></p>
             </footer>
